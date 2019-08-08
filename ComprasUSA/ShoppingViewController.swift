c471e3ab9a0eb8eb3f1fcac5e408b15ed3b257dc
BrunoALuiz <>
2019-08-07 22:52:36 -0300

    First commit

---
+++ ComprasUSA/ShoppingViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ComprasUSA
+//
+//  Created by Bruno Luiz on 03/07/19.
+//  Copyright © 2019 Bruno Luiz. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ComprasUSA/TaxesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TaxesViewController.swift
+//  ComprasUSA
+//
+//  Created by Bruno Luiz on 03/07/19.
+//  Copyright © 2019 Bruno Luiz. All rights reserved.
+//
+
+import UIKit
+
+class TaxesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
